36c5ba39d829b66aba514fe9dbbba680f86b62a7
Elise Poplawski <elise.poplawski@gmail.com>
2017-08-13 05:36:56 -0500

    add warp_to method and driver code to change location

--- spaceship.rb
+++ spaceship.rb
@@ -3,13 +3,14 @@
 
 class Spaceship
 
-	attr_reader :max_speed
-	attr_accessor :name, :location
+	attr_reader :max_speed, :location
+	attr_accessor :name 
 
 	def initialize(name, max_speed)
 		@name = name
 		@max_speed = max_speed
 		@location = location
+		
 	end
 
 	def disable_shield
@@ -21,7 +22,7 @@ class Spaceship
 	end
 
 	def warp_to(location)
-		puts "Taveling at #{@max_speed} to #{location}"
+		puts "Taveling at #{@max_speed} mph to #{location}"
 	end
 end
 
@@ -32,7 +33,7 @@ p spaceship = Spaceship.new("USS Enterprise", 200_000)
 #p spaceship.max_speed
 #p spaceship.enable_shield
 #p spaceship.disable_shield
-spaceship.location("Wisconsin")
+spaceship.warp_to("Wisconsin")
 p spaceship = Spaceship.new("HMS Andromeda", 108_277)
 #p spaceship.name
 #spaceship.name= ("little_guy")
