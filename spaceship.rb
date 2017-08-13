3d0410c4afddd84a139eebaf3756774ffb56edfc
Elise Poplawski <elise.poplawski@gmail.com>
2017-08-13 05:29:05 -0500

    added shield methods and driver code

--- spaceship.rb
+++ spaceship.rb
@@ -10,14 +10,24 @@ class Spaceship
 		@name = name
 		@max_speed = max_speed
 	end
+
+	def disable_shield
+		"The shields are now down! Be careful!"
+	end
+
+	def enable_shield
+		"The shields are now up!"
+	end
 end
 
 ## driver code
 
 p spaceship = Spaceship.new("USS Enterprise", 200_000)
-p spaceship.name
-p spaceship.max_speed
+#p spaceship.name
+#p spaceship.max_speed
+p spaceship.enable_shield
+p spaceship.disable_shield
 p spaceship = Spaceship.new("HMS Andromeda", 108_277)
 p spaceship.name
-spaceship.name= ("little_guy")
-p spaceship.name
+#spaceship.name= ("little_guy")
+#p spaceship.name
