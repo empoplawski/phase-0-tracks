b15bc742cb56c409bc3b2def5c98103e2bb124e1
Elise Poplawski <elise.poplawski@gmail.com>
2017-08-13 06:52:31 -0500

    recommit of just class definitition and initialize method

--- spaceship.rb
+++ spaceship.rb
@@ -3,90 +3,19 @@
 
 class Spaceship
 
-	attr_reader :max_speed, :location, :inventory
-	attr_accessor :name 
 
 	def initialize(name, max_speed)
 		@name = name
 		@max_speed = max_speed
-		@location = location
-		@inventory = {}
-		
 	end
-
-	def disable_shield
-		"The shields are now down! Be careful!"
-	end
-
-	def enable_shield
-		"The shields are now up!"
-	end
-
-	def warp_to(location)
-		puts "Taveling at #{@max_speed} mph to #{location}"
-		@current_location = location
-	end
-
-	def tractor_beam(item)
-		disable_shield
-		item_weight = item.chars.map { |c| c.ord}.inject(:+)
-
-		if item_weight <= 500
-			@inventory[item] = @current_location
-			true
-
-		else enable_shield
-			false
-		end
-	end
-
-	def pickup(item, location)
-		warp_to(location)
-		tractor_beam(item)
-	end
-
-	def print_inventory
-		puts
-		puts "In your inventory you have:"
-		puts
-		@inventory.each do |item, location|
-			puts "- A(n) #{item} from #{location}"
-		puts
-		end
-	end
-
-	def total_weight
-		@inventory.keys.join.chars.map { |c| c.ord }.inject(:+)
-	end
-
 end
-
-
 
 
 
 ## driver code
 
 p spaceship = Spaceship.new("USS Enterprise", 200_000)
-#p spaceship.name
-#p spaceship.max_speed
-#p spaceship.enable_shield
-#p spaceship.disable_shield
-# spaceship.warp_to("Wisconsin")
-#p spaceship.tractor_beam("cow")
-
-spaceship.pickup("cow", "toledo")
-spaceship.pickup("semi_truck", "iowa")
-spaceship.pickup("duck", "illinois")
-spaceship.pickup("ox", "mars")
-spaceship.print_inventory
-p spaceship.total_weight
-
-
 
 p spaceship = Spaceship.new("HMS Andromeda", 108_277)
-#p spaceship.name
-#spaceship.name= ("little_guy")
-#p spaceship.name
 
 
