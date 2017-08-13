74cdff61222eeb617eaa2935ec5a09a9c3b46065
Elise Poplawski <elise.poplawski@gmail.com>
2017-08-13 05:31:56 -0500

    added shield methods and driver code

--- spaceship.rb
+++ spaceship.rb
@@ -4,11 +4,12 @@
 class Spaceship
 
 	attr_reader :max_speed
-	attr_accessor :name
+	attr_accessor :name, :location
 
 	def initialize(name, max_speed)
 		@name = name
 		@max_speed = max_speed
+		@location = location
 	end
 
 	def disable_shield
@@ -18,6 +19,10 @@ class Spaceship
 	def enable_shield
 		"The shields are now up!"
 	end
+
+	def warp_to(location)
+		puts "Taveling at #{@max_speed} to #{location}"
+	end
 end
 
 ## driver code
@@ -25,9 +30,10 @@ end
 p spaceship = Spaceship.new("USS Enterprise", 200_000)
 #p spaceship.name
 #p spaceship.max_speed
-p spaceship.enable_shield
-p spaceship.disable_shield
+#p spaceship.enable_shield
+#p spaceship.disable_shield
+spaceship.location("Wisconsin")
 p spaceship = Spaceship.new("HMS Andromeda", 108_277)
-p spaceship.name
+#p spaceship.name
 #spaceship.name= ("little_guy")
 #p spaceship.name
