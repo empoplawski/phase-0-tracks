7831a6e6dc5a7effab2988d541b4c7a043e205aa
Elise Poplawski <elise.poplawski@gmail.com>
2017-08-13 05:23:42 -0500

    added attr_readers and accessors with driver code for name and max speed

--- spaceship.rb
+++ spaceship.rb
@@ -3,6 +3,9 @@
 
 class Spaceship
 
+	attr_reader :max_speed
+	attr_accessor :name
+
 	def initialize(name, max_speed)
 		@name = name
 		@max_speed = max_speed
@@ -12,4 +15,9 @@ end
 ## driver code
 
 p spaceship = Spaceship.new("USS Enterprise", 200_000)
+p spaceship.name
+p spaceship.max_speed
 p spaceship = Spaceship.new("HMS Andromeda", 108_277)
+p spaceship.name
+spaceship.name= ("little_guy")
+p spaceship.name
